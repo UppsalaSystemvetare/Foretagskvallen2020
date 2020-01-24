26401569a9997ce56caa046103c95486a71dcb75
Vildmusen <viktorvilmusenaho@hotmail.com>
2020-01-24 21:21:14 +0100

    drag and drop funkar ej med touch

--- include/html/default.php
+++ include/html/default.php
@@ -13,5 +13,5 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
         <script src="https://code.jquery.com/jquery-2.2.0.js"></script>
-    <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
     </head>
