2f5b0811a99cdf9db6b14005f6d9a240a58d4a35
kvacke <jens.bleckert@gmail.com>
2020-01-27 20:59:25 +0100

    la till overlay

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,39 @@
 jQuery(function($) {
-    var panelList = $('#draggablePanelList');
+
+    /*
     
+    Implementerad funktionalitet:
+    1.  En välkomst-"overlay" i form av en 'modal' från bootstrap som visar ett välkomstmeddelande
+        och promptar användaren att fylla i namn. Kan ej klickas ner utan att ange giltigt namn.
+
+    2. Namnet visas högst upp.
+
+    3. En sorterbar draggable med index för respektive företag
+    
+    
+    Att göra:
+        Php-sessions: Kom ihåg namnet och hindra popupen från att dyka upp vid refresh.
+        Exportera den valda listordningen till databasen
+    */
+
+
+
+    
+    
+    
+    var nameinput = $("#InputName"); //textfältet för namn
+    var contbutton = $("#contbutton"); //gå vidare-knappen
+    var name = "";  //namn-variabel till databasen och front-end
+    
+    contbutton.prop("disabled", checkInputs()); //gå vidare-knappen låst som default
+    $('#Modal').modal('show'); //popup-fönstret visas som default,
+
+    $('#Modal').modal({
+        keyboard: false
+      })
+
+
+    var panelList = $('#draggablePanelList');
     panelList.sortable({
         // Only make the .panel-heading child elements support dragging.
         // Omit this to make then entire <li>...</li> draggable.
@@ -14,4 +47,33 @@ jQuery(function($) {
             });
         }
     });
+ 
+    // om 'Gå vidare'-knappen trycks in döljs fönstret, name-variabeln tilldelas värdet från textfältet, namnet visas för användaren.
+    $('#contbutton').click(function(){
+        $('#Modal').modal('hide');
+        name = $("#InputName").val();
+        $('#nametag').html(name);
+
+    })
+
+    //Varje knapptryck: kolla om gå vidare-knappen ska fortsätta vara låst med checkInputs()
+    $("#InputName").keyup(function(){
+        contbutton.prop("disabled", checkInputs());
+    })
+    
+    //False om följande är sant: (innehåller whitespace && första tecknet och sista tecknet är inte whitespace && textinput är längre än 5)
+    function checkInputs(){
+            return !((/\s/.test(nameinput.val())) && 
+                     !(/\s/.test(nameinput.val()[nameinput.val().length -1])) &&
+                     !(/\s/.test(nameinput.val()[0])) &&
+                     (nameinput.val().length > 5));
+    }
+
 });
+
+
+
+
+
+
+
--- login.php
+++ login.php
@@ -10,15 +10,15 @@
     <body>
         <div class="container">
             <h1>Login</h1>
-            <div class="form-group">
-                <input id="NAME" type="email" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="email">
-            </div>
-            <div class="form-group">
-                <input id="PASS" type="password" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="password">  
-            </div>
-            <div class="form-group">
-                <button class="btn btn-primary" type="submit" value="Submit" onclick="login()">Logga in</button>
-            </div>
+                <div class="form-group">
+                    <input id="NAME" type="email" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="email">
+                </div>
+                <div class="form-group">
+                    <input id="PASS" type="password" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="password">  
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary" type="submit" value="Submit" onclick="login()">Logga in</button>
+                </div>
             </form> 
         </div>
     </body>
