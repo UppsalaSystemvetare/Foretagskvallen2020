be6a79ace3dd997b6b0457b47c30e8c7b26f589a
Vildmusen <viktorvilmusenaho@hotmail.com>
2020-01-31 15:11:07 +0100

    användarnamn sparas i session

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,25 @@
+var nameinput;
+var contbutton;
+var name;
+
+function checkPopup(){
+    nameinput = $("#InputName"); //textfältet för namn
+    contbutton = $("#contbutton"); //gå vidare-knappen
+    name = "";  //namn-variabel till databasen och front-end
+    displayname = $("#nametag").html();
+
+    if(!displayname || displayname === 0){
+        contbutton.prop("disabled", checkInputs()); //gå vidare-knappen låst som default
+            
+        $('#Modal').modal('hide'); //popup-fönstret visas om namn inte är satt
+        
+        $('#Modal').modal({
+            keyboard: false
+        });
+    }
+}
+
 jQuery(function($) {
-
-    /*
-    
-    Implementerad funktionalitet:
-    1.  En välkomst-"overlay" i form av en 'modal' från bootstrap som visar ett välkomstmeddelande
-        och promptar användaren att fylla i namn. Kan ej klickas ner utan att ange giltigt namn.
-
-    2. Namnet visas högst upp.
-
-    3. En sorterbar draggable med index för respektive företag
-    
-    
-    Att göra:
-        Php-sessions: Kom ihåg namnet och hindra popupen från att dyka upp vid refresh.
-        Exportera den valda listordningen till databasen
-    */
-
-
-
-    
-    
-    
-    var nameinput = $("#InputName"); //textfältet för namn
-    var contbutton = $("#contbutton"); //gå vidare-knappen
-    var name = "";  //namn-variabel till databasen och front-end
-    
-    contbutton.prop("disabled", checkInputs()); //gå vidare-knappen låst som default
-    $('#Modal').modal('show'); //popup-fönstret visas som default,
-
-    $('#Modal').modal({
-        keyboard: false
-      })
-
-
     var panelList = $('#draggablePanelList');
     panelList.sortable({
         // Only make the .panel-heading child elements support dragging.
@@ -47,29 +34,44 @@ jQuery(function($) {
             });
         }
     });
- 
-    // om 'Gå vidare'-knappen trycks in döljs fönstret, name-variabeln tilldelas värdet från textfältet, namnet visas för användaren.
-    $('#contbutton').click(function(){
-        $('#Modal').modal('hide');
-        name = $("#InputName").val();
-        $('#nametag').html(name);
-
-    })
-
-    //Varje knapptryck: kolla om gå vidare-knappen ska fortsätta vara låst med checkInputs()
-    $("#InputName").keyup(function(){
-        contbutton.prop("disabled", checkInputs());
-    })
-    
-    //False om följande är sant: (innehåller whitespace && första tecknet och sista tecknet är inte whitespace && textinput är längre än 5)
-    function checkInputs(){
-            return !((/\s/.test(nameinput.val())) && 
-                     !(/\s/.test(nameinput.val()[nameinput.val().length -1])) &&
-                     !(/\s/.test(nameinput.val()[0])) &&
-                     (nameinput.val().length > 5));
-    }
-
 });
+
+
+// om 'Gå vidare'-knappen trycks in döljs fönstret, name-variabeln tilldelas värdet från textfältet, namnet visas för användaren.
+$('#contbutton').click(function(){
+    $('#Modal').modal('hide');
+    name = $("#InputName").val();
+    $('#nametag').html(name);
+    SetNameSession(name);
+})
+
+//Varje knapptryck: kolla om gå vidare-knappen ska fortsätta vara låst med checkInputs()
+$("#InputName").keyup(function(){
+    contbutton.prop("disabled", checkInputs());
+})
+
+//False om följande är sant: (innehåller whitespace && första tecknet och sista tecknet är inte whitespace && textinput är längre än 5)
+function checkInputs(){
+        return !((/\s/.test(nameinput.val())) && 
+                    !(/\s/.test(nameinput.val()[nameinput.val().length -1])) &&
+                    !(/\s/.test(nameinput.val()[0])) &&
+                    (nameinput.val().length > 5));
+}
+
+function SetNameSession(nameval) {
+    var payload = { name: nameval };
+    $.post(
+        "include/models/set_name.php",
+        payload,
+        function successLogin(data, status, xhr){
+            // window.location.href = "index.php";
+        }
+    );
+}
+
+function HideNamePopUp(){
+    $('#Modal').modal('hide'); 
+}
 
 
 
--- index.php
+++ index.php
@@ -1,60 +1,60 @@
 <?php
+session_start();
 include("include/html/default.php");
+
 ?>
-    <body>
+    <body onload="checkPopup();">
+        <!-- Modal -->
+        <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-lg" role="document">
+                <div class="modal-content">
 
-                <!-- Modal -->
-                <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static">
-                    <div class="modal-dialog modal-dialog-centered modal-dialog-lg" role="document">
-                        <div class="modal-content">
-
-                        <div class="modal-body">
-                            <div class="container-md">
-                                <h1>Välkommen till företagskvällen!</h1>
-                                <div class="intro-text">
-                                    <p>Under företagens presentationer kommer du att kunna välja vilket företag du helst vill besöka senare.
-                                    För att vi ska kunna placera dig i systemet behöver vi veta vad du heter.</p>
-                                    <p> Var vänlig fyll i både för- och efternamn :</p>
-                                    <br>
-                                    <form>
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" id="InputName" placeholder="För- och efternamn">
-                                            <small id="emailHelp" class="form-text text-muted">Namnet används vid tilldelning av företagscase och kan 
-                                            ej ändras i efterhand - så stava rätt!</small>
-                                        </div>
-                                    </form>
+                <div class="modal-body">
+                    <div class="container-md">
+                        <h1>Välkommen till företagskvällen!</h1>
+                        <div class="intro-text">
+                            <p>Under företagens presentationer kommer du att kunna välja vilket företag du helst vill besöka senare.
+                            För att vi ska kunna placera dig i systemet behöver vi veta vad du heter.</p>
+                            <p> Var vänlig fyll i både för- och efternamn :</p>
+                            <br>
+                            <form>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="InputName" placeholder="För- och efternamn">
+                                    <small id="emailHelp" class="form-text text-muted">Namnet används vid tilldelning av företagscase och kan 
+                                    ej ändras i efterhand - så stava rätt!</small>
                                 </div>
-                                
-                            </div>
+                            </form>
                         </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-primary" id="contbutton">Gå vidare</button>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
-
-
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" id="contbutton">Gå vidare</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <div class="foretag-wrapper">
-            <br>
-            <div>
-            Anmäld som:
-            </div>
-            <div class="h1 text-primary" id="nametag">
+            <div class="name-holder">
+                <br>
+                <div>
+                    Anmäld som:
+                </div>
+                <div class="h1 text-primary" id="nametag"><?php if(isset($_SESSION['name']) && !empty($_SESSION['name'])) { echo $_SESSION['name']; } ?></div>
             </div>
             <br><br>
             <h2>Dags att välja företag!</h2>
             <div class="intro-text">
-                    <p>
-                    Flytta runt de 5 företagen och placera de du helst vill besöka högst upp! <br>
-                    Du kan flytta runt dem hur många gånger som helst
-                    under presentationerna och du kommer att förvarnas innan
-                    valen blir låsta. 
-                    </p>
-                </div>
+                <p>
+                Flytta runt de 5 företagen och placera de du helst vill besöka högst upp! 
+                Du kan flytta runt dem hur många gånger som helst
+                under presentationerna och du kommer att förvarnas innan
+                valen blir låsta. 
+                </p>
+            </div>
+
             <div class="foretag p-3 border bg-light">
-                
 
                 <p class="small-p">Dra och släpp företagen i önskad ordning.</p>
 
@@ -77,6 +77,9 @@ include("include/html/default.php");
                 </ul>
 
             </div>
+
+            <button type="button" class="btn btn-primary centered" id="contbutton">Gå vidare</button>
+
         </div>
 
     </body>
