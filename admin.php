e5f33daaa51cec936d212e4744ec08e6b4f90c01
HannesSam <hannessamskog@gmail.com>
2020-01-31 16:34:22 +0100

    First draft

--- admin.php
+++ admin.php
@@ -2,11 +2,12 @@
 include("include/models/header.php");
 include("include/html/default.php");
 ?>
-
-
     <body>
 
         <div class="container">
+        <?php  
+        require "include/models/sortingScript.php";
+        ?>
             wassup
         </div>
 
