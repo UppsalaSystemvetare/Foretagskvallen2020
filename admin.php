bab1f60a8f71bb1cf224a9297a9386a630933ebe
Christoffer Wrigmar <christoffer.wrigmar@gmail.com>
2020-02-06 17:41:07 +0100

    Ville ha-funktion
    Räknare för hur många som valde de enskilda företagen som första- eller andrahandsval.
    
    Returnerar tabell med företagsnamn och antal.

--- admin.php
+++ admin.php
@@ -3,14 +3,13 @@ include("include/models/header.php");
 include("include/html/default.php");
 ?>
     <body>
-
         <div class="container">
-        <?php  
-        require "include/models/sortingScript.php";
+        <?php
+            include "include/models/check_assign.php";  
+        // require "include/models/sortingScript.php";
         ?>
-            wassup
+        
         </div>
-
         <div class="testData_button">
                 <button type="button" class="btn btn-primary" onclick="generateTestData()">Generera testdata</button>
             </div>
---
+++ include/models/check_assign.php
@@ -0,0 +1,47 @@
+<?php
+
+    $firstorsecond = [];
+    $foretag = [];
+
+
+    $connection = connect();
+
+    $query = "SELECT foretag_id, foretag_name FROM foretag ORDER BY foretag_id";
+    $result = $connection->query($query);
+
+    while($foretag_data = mysqli_fetch_array($result)){
+        $foretag_name = $foretag_data["foretag_name"];
+        $foretag [] = $foretag_name; //Lägger till företagsnamnet
+        $firstorsecond [] = 0; //Lägger till en räknare för varje företag
+    }
+
+
+    $query = "SELECT user_picks FROM user_picks";
+    $result = $connection->query($query);
+
+    while($picks = mysqli_fetch_array($result)){
+        $user_picks = $picks["user_picks"];
+        for ($i=0; $i < strlen($user_picks); $i++) { 
+            if(substr($user_picks, $i, 1) == "1" || substr($user_picks, $i, 1) == "2"){ //Kontrollerar om "företag i" är någons första- eller andrahandsval
+                $firstorsecond[$i] += 1;
+            }
+        }
+    }
+
+    $connection = disconnect();
+?>
+
+
+<table>
+    <tr>
+        <th>Företag</th>
+        <th>Antal som ville ha som första- eller andrahandsval</th>
+    </tr>
+        <?php
+            for ($i=0; $i < count($foretag); $i++) : ?>
+                <tr>
+                    <td><?php echo $foretag[$i]; ?> </td>
+                    <td><?php echo $firstorsecond[$i] ?> </td>
+                </tr>
+            <?php endfor; ?>
+</table>
