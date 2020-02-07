89ce951e61224dbdc4e919c8619f9a9f952aa147
Vildmusen <viktorvilmusenaho@hotmail.com>
2020-02-07 14:47:27 +0100

    javascript är det bästa som finns

--- assets/js/index.js
+++ assets/js/index.js
@@ -83,8 +83,3 @@ function readFromList(){
     }
     return { name: nameval, order: choices };
 }
-
-
-
-
-
--- include/models/testData.php
+++ include/models/testData.php
@@ -1,13 +1,12 @@
 <?php
-include "header.php";
-//lägger till x många användare i databasen med randomiserade val. 
-$numberOfUsers = 100;
+
+$numberOfUsers = 100
 
 $connection = connect();
 for ($i = 1; $i <= $numberOfUsers; $i++) { 
 
     $user_name = "Test User" . $i;
-    $choices = str_shuffle("12345");
+    $choices = "12345";
     $query = "INSERT INTO user_picks (user_name, user_picks) VALUES ('$user_name', '$choices')";
     $result = $connection->query($query);
 }
